751c347e3ac53205f35f59769147bc059bdd0904
Marek Belisko <marek.belisko@gmail.com>
2009-09-02 13:32:27 +0200

    Minor updates. Comment updates. Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

--- iqrf/iqrf.h
+++ iqrf/iqrf.h
@@ -6,13 +6,8 @@
 extern uint8_t iqrfTx[35], iqrfRx[35];
 volatile extern uint8_t fRxIQRF, fErrIQRF, IQRF_ErrCount;
 
-// odeslani zpravy dane delky, fce se postara o rezijni byty
-extern uint8_t iqrf_send_buff(uint8_t *iqrfTx,uint8_t delka);
-
-// odeslani jednoho bytu
+extern uint8_t iqrf_send_buff(uint8_t *iqrfTx,uint8_t len);
 extern uint8_t iqrf_send_byte(uint8_t cData);
-
-// kontrola stavu IQRF
 extern uint8_t iqrf_status(void);
 
 #endif
